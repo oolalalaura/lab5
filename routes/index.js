bae44d3a1d74e4805f0beb9755de7f8b62a66d43
oolalalaura <lchavess@ucsd.edu>
2014-02-07 01:10:49 -0800

    putting it together lab

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,21 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	res.render('index', data
+	);
+function initializePage() {
+	console.log("Javascript Connected!");
+	$("a.nameListener").click(nameClick);
+}
+
+function nameClick(e) {
+	console.log("Name clicked");
+	e.preventDefault();
+
+	var currentName = $(this).text();
+	console.log(currentName);
+	$(this).text(anagrammedName(currentName));
+}
+
 };
+
